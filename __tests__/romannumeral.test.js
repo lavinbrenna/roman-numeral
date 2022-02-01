8dfecb69eb00990627d75b6903ef27d8114a253f
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-01 10:11:17 -0800

    add test to convert 1 to I, pass
    Co-authored-by: Will Greenberg <greenberg.will@gmail.com>

--- __tests__/romannumeral.test.js
+++ __tests__/romannumeral.test.js
@@ -8,3 +8,12 @@ describe('RomanNumeral', () =>{
     expect(romanNumeral.number).toEqual(2);
   });
 });
+
+describe('RomanNumeral.convertNumber', () =>{
+
+  test('should convert 1 to I', () => {
+    const romanNumeral = new RomanNumeral(1);
+    expect(romanNumeral.convertNumber()).toEqual("I");
+  });
+  
+});
--- src/romanNumeral.js
+++ src/romanNumeral.js
@@ -1,4 +1,12 @@
 export default function RomanNumeral(number){
-this.number = number;
-return this.number;
+  this.number = number;
+  return this.number;
 }
+
+RomanNumeral.prototype.convertNumber = function () {
+  if (this.number === 1) {
+    return "I";
+  } else {
+    return "not I";
+  }
+};
